39546f5908e739d655a0b50aa0b934e96a177a09
Banele <skosanabanele6@gmail.com>
2026-01-17 17:32:50 +0200

    food-website

---
+++ food-website/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector('.sidebar');
+
+function openSidebar() {
+    sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+    sidebar.style.display = 'none';
+}
